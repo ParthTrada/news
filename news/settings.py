3afddeade0e98ec42ff258df48f6ab7f05ea4fdf
Parth Trada <42438040+ParthTrada@users.noreply.github.com>
2020-10-19 01:53:25 +0530

    Add files via upload

--- news/settings.py
+++ news/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 't9)+7(29(ll+wq8h#*e&^vos-!)3m*-qf$y-j(vwgv_&2usgyd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '127.0.0.1','newsblog2507.herokuapp.com' ]
 
